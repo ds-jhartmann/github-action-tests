1de83887da88ebffa528a6ed6a6c37d1592b576b
Jaro Hartmann <jaro.hartmann@doubleslash.de>
2023-12-21 09:56:34 +0100

    feat(cucumber): Rename cucumber result file

--- src/test/java/de/doubleslash/cucumber/RunCucumberTest.java
+++ src/test/java/de/doubleslash/cucumber/RunCucumberTest.java
@@ -16,7 +16,7 @@ import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PUBLISH_QUIET_P
 @ConfigurationParameter(key = PLUGIN_PUBLISH_ENABLED_PROPERTY_NAME, value = "true")
 @ConfigurationParameter(
     key = PLUGIN_PROPERTY_NAME,
-    value = "junit:target/junit.xml,summary")
+    value = "junit:target/cucumber-junit.xml,summary")
 public class RunCucumberTest {
   // This is a setup Class and supposed to be empty
 }
