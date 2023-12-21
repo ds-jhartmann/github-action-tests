eb8101653a5c289ff6655b47fb355deb0fd4a69c
Jaro Hartmann <jaro.hartmann@doubleslash.de>
2023-12-21 09:34:00 +0100

    feat(cucumber): Remove unneeded test result files

--- src/test/java/de/doubleslash/cucumber/RunCucumberTest.java
+++ src/test/java/de/doubleslash/cucumber/RunCucumberTest.java
@@ -13,10 +13,10 @@ import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PUBLISH_QUIET_P
 @IncludeEngines("cucumber")
 @SelectClasspathResource("features")
 @ConfigurationParameter(key = PLUGIN_PUBLISH_QUIET_PROPERTY_NAME, value = "false")
-@ConfigurationParameter(key = PLUGIN_PUBLISH_ENABLED_PROPERTY_NAME, value = "false")
+@ConfigurationParameter(key = PLUGIN_PUBLISH_ENABLED_PROPERTY_NAME, value = "true")
 @ConfigurationParameter(
     key = PLUGIN_PROPERTY_NAME,
-    value = "junit:target/junit.xml,summary,json:target/report.json,pretty:target/pretty")
+    value = "junit:target/junit.xml,summary")
 public class RunCucumberTest {
   // This is a setup Class and supposed to be empty
 }
