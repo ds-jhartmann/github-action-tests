3d216c09653d0100858e231d410e0cdd4761fbf0
Jaro Hartmann <57985712+ds-jhartmann@users.noreply.github.com>
2024-06-10 10:44:49 +0200

    disable ANSI colors in cucumber logs

--- src/test/java/de/doubleslash/cucumber/RunCucumberTest.java
+++ src/test/java/de/doubleslash/cucumber/RunCucumberTest.java
@@ -5,6 +5,7 @@ import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
+import static io.cucumber.junit.platform.engine.Constants.ANSI_COLORS_DISABLED_PROPERTY_NAME;
 import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PUBLISH_ENABLED_PROPERTY_NAME;
 import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PUBLISH_QUIET_PROPERTY_NAME;
@@ -14,6 +15,7 @@ import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PUBLISH_QUIET_P
 @SelectClasspathResource("features")
 @ConfigurationParameter(key = PLUGIN_PUBLISH_QUIET_PROPERTY_NAME, value = "false")
 @ConfigurationParameter(key = PLUGIN_PUBLISH_ENABLED_PROPERTY_NAME, value = "true")
+@ConfigurationParameter(key = ANSI_COLORS_DISABLED_PROPERTY_NAME, value = "true")
 @ConfigurationParameter(
     key = PLUGIN_PROPERTY_NAME,
     value = "junit:target/cucumber-junit.xml,summary")
